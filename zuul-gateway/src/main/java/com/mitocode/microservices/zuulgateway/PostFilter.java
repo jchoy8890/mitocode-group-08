0bd16fee1ddb7c70cad2de00f3dfc36a63a5fbfc
Jonathan Choy <jchoy8890@gmail.com>
2024-12-11 00:44:58 -0500

    Upload Zuul Gateway

---
+++ zuul-gateway/src/main/java/com/mitocode/microservices/zuulgateway/PostFilter.java
@@ -0,0 +1,39 @@
+package com.mitocode.microservices.zuulgateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class PostFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        Long elapsedTime = System.currentTimeMillis() - (Long) request.getAttribute("startTime");
+        log.info(String.format("Time elapsed in this request %s ms", elapsedTime));
+        return null;
+    }
+
+}
---
+++ zuul-gateway/src/main/java/com/mitocode/microservices/zuulgateway/PreFilter.java
@@ -0,0 +1,39 @@
+package com.mitocode.microservices.zuulgateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class PreFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        log.info(String.format("%s request router to %s", request.getMethod(), request.getRequestURL().toString()));
+        Long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return null;
+    }
+}
